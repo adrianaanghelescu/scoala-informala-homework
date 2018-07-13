fe7d285696c46fda9955fd281d6d58a222487f8c
luci <adriana.anghelescu83@yahoo.com>
2018-07-13 22:48:53 +0300

    test commit

--- bv-java-grupa-2/HOMEWORK1/src/Problemaunu.java
+++ bv-java-grupa-2/HOMEWORK1/src/Problemaunu.java
@@ -1,16 +1,14 @@
 
+// 1.	Calculate the sum of the first 100 numbers higher than 0
+
 
 public class Problemaunu {
-	public Problemaunu() {
-			System.out.println("Programul face suma primelor 100 de numere");
-			int i, sum = 0;
-			for(i=1;i<=100;i++) {
-				sum = sum + i;
-			}
-			System.out.println("Suma primelor 100 de numere este:"+sum);
+    public static void main(String[] args) {
+		int i, Sum = 0;
+		for(i=1;i<=100;i++) {
+			Sum = Sum + i;
 		}
+	System.out.println("Suma primelor 100 de numere este:"+Sum);
 	}
-
-
-
+}
 
